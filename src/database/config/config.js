fa642241a406a7fbc082e12d3f89035ae8ec4ba2
LucioMart <luchoweb15@gmail.com>
2021-12-06 00:32:27 -0300

    manipulación de datos.

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "L19862022o.",
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port":3306
+  },
+  "test": {
+    "username": "root",
+    "password": "L19862022o.",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "L19862022o.",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
